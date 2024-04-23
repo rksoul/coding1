0a7f1e805fa4c5dff78c22e5cec2fb3de90c39c3
rksoul <167795238+rksoul@users.noreply.github.com>
2024-04-23 17:28:50 +0900

    name.php

---
+++ name.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="login_css">
+    <title>로그인</title>
+</head>
+<body>
+    <h1>로그인</h1>
+    <form method="POST" action="login_proc.php">
+        <input= type="text" name="id" placeholder="User ID"/>
+        <input= type="password" name="pass" placeholder="User password"/>
+        <input= type="submit" value="login"/>
+
+
+    </form>
+    
+</body>
+</html>
